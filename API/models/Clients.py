866bc61b81cac242371e8e15972205fd833e9aea
Jose Andres Sierra Arrieta <joseandresmayo@gmail.com>
2022-11-13 23:46:34 -0500

    feat: Clients model added

---
+++ API/models/Clients.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from base_model.models import AbstractModel
+
+TYPE_DOCUMENT_CHOICES = (
+    (_('ID'), _('ID Card')),
+    (_('CI'), _('Card Identification')),
+    (_('NIT'), _('NIT'))
+)
+
+
+class Clients(AbstractModel):
+    """
+    Model which contains information about clients
+    """
+    document = models.PositiveIntegerField(verbose_name=_('Document'))
+    type_document = models.CharField(
+        verbose_name=_('Type document'), max_length=3, choices=TYPE_DOCUMENT_CHOICES
+    )
+    first_name = models.CharField(verbose_name=_('First name'), max_length=50)
+    last_name = models.CharField(verbose_name=_('Last name'), max_length=50)
+    email = models.EmailField(verbose_name=_('Email'), max_length=150)
---
+++ API/models/__init__.py
@@ -0,0 +1,1 @@
+from .Clients import Clients
