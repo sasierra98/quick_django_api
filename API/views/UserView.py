56e97a2a0eaccee6f2663c366e3502b4a866a4ef
Jose Andres Sierra Arrieta <joseandresmayo@gmail.com>
2022-11-14 19:52:12 -0500

    feat: Views of user added

---
+++ API/views/UserView.py
@@ -0,0 +1,67 @@
+from rest_framework import status
+from rest_framework.generics import get_object_or_404
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import AllowAny
+
+from API.models import User
+from API.serializers import UserSerializer
+
+
+class UserView(APIView):
+    def get(self, request: Request, format=None) -> Response:
+        user = User.objects.all()
+        serializer = UserSerializer(user, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class UserDetailView(APIView):
+    def get(self, request: Request, user_id: int, format=None) -> Response:
+        user = get_object_or_404(User, pk=user_id)
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request: Request, user_id: int, format=None) -> Response:
+        user = get_object_or_404(User, pk=user_id)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request: Request, user_id: int, format=None) -> Response:
+        user = get_object_or_404(User, pk=user_id)
+        serializer = UserSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request: Request, user_id: int, format=None):
+        user = get_object_or_404(User, pk=user_id)
+        user.delete()
+        return Response(
+            {"status": "Deleted", "data": request.data}, status=status.HTTP_204_NO_CONTENT
+        )
+
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+
+
+class RegisterView(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request: Request, format=None) -> Response:
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
