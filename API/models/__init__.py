48daedc651fd7312fa2230c61f290ea0878b861d
Jose Andres Sierra Arrieta <joseandresmayo@gmail.com>
2022-11-14 19:49:08 -0500

    feat: Abstract setting added in meta of AbstractModel

--- API/models/__init__.py
+++ API/models/__init__.py
@@ -1,3 +1,4 @@
+from .User import User
 from .Clients import Clients
 from .Products import Products
 from .Bills import Products
--- base_model/models/AbstractModel.py
+++ base_model/models/AbstractModel.py
@@ -5,5 +5,9 @@ class AbstractModel(models.Model):
     """
     Abstract model used to provide essential fields to models
     """
+    is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
