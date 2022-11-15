f2496acc30e5003479938629d6734c3940f9b75f
Jose Andres Sierra Arrieta <joseandresmayo@gmail.com>
2022-11-14 19:02:41 -0500

    feat: User serializer added

---
+++ API/serializers/UserSerializer.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from API.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        user = User.objects.create(email=validated_data['email'])
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    class Meta:
+        model = User
+        fields = ('email', 'password')
